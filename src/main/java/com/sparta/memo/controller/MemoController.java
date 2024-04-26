7a914eadaf4e4756641744e07000a8ca64c197f7
lgsok00 <lgsok00@naver.com>
2024-04-26 16:21:19 +0900

    강한 결합 문제 수정

--- src/main/java/com/sparta/memo/controller/MemoController.java
+++ src/main/java/com/sparta/memo/controller/MemoController.java
@@ -3,7 +3,6 @@ package com.sparta.memo.controller;
 import com.sparta.memo.dto.MemoRequestDto;
 import com.sparta.memo.dto.MemoResponseDto;
 import com.sparta.memo.service.MemoService;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -21,8 +20,8 @@ public class MemoController {
 
   private final MemoService memoService;
 
-  public MemoController(JdbcTemplate jdbcTemplate) {
-    this.memoService = new MemoService(jdbcTemplate);
+  public MemoController(MemoService memoService) {
+    this.memoService = memoService;
   }
 
   @PostMapping("/memos")
